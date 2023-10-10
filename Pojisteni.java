7b598df2f137a57b381b1c751b82172ca6e0f981
Miroslav 'Spike' Tomáš <137869807+Spikehardcore@users.noreply.github.com>
2023-10-10 12:09:36 +0200

    Update Pojisteni.java

--- Pojisteni.java
+++ Pojisteni.java
@@ -1,9 +1,10 @@
 public class Pojisteni {
-    private String jmeno;
-    private String prijmeni;
-    private int vek;
-    private int telefonniCislo;
+    private String jmeno; // Jméno pojištěného.
+    private String prijmeni; // Příjmení pojištěnéh.
+    private int vek; // Věk pojištěného.
+    private int telefonniCislo; // Telefonní číslo pojištěného.
 
+    // Konstruktor pro inicializaci dat pojištěného.
     public Pojisteni(String jmeno, String prijmeni, int vek, int telefonniCislo) {
         this.jmeno = jmeno;
         this.prijmeni = prijmeni;
@@ -11,22 +12,27 @@ public class Pojisteni {
         this.telefonniCislo = telefonniCislo;
     }
 
+    // Metoda pro získání jména pojištěného.
     public String getJmeno() {
         return jmeno;
     }
 
+    // Metoda pro získání příjmení pojištěného.
     public String getPrijmeni() {
         return prijmeni;
     }
 
+    // Metoda pro získání věku pojištěného.
     public int getVek() {
         return vek;
     }
 
+    // Metoda pro získání telefonního čísla pojištěného.
     public int getTelefonniCislo() {
         return telefonniCislo;
     }
 
+    // Přetížená metoda toString pro vytvoření textového reprezentace objektu.
     @Override
     public String toString() {
         return jmeno + "   " + prijmeni + "   " + vek + "   " + telefonniCislo;
